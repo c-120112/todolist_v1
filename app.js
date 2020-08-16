5bfe9fb9171644757f01d9aacde6176ecd0ef65e
Cory Risch-Smith <Corys_Macbook_Pro@devcor-MacBook-Pro.local>
2020-08-16 00:21:55 -0700

    updated files, app.js

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
 //starting out using an array to store the new items so that there can be multiple entries.
-let items = [];
-
+const items = [];
+const workItems = [];
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -15,31 +17,13 @@ app.use(bodyParser.urlencoded({
 
 //Telling our server to serve up the public folder as a static resource.
 app.use(express.static("public"));
-
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
 
-  let today = new Date();
+let day = date.getDate();
 
-  //options for displayed day/date/time,etc. is a JS object
-  //see https://stackoverflow.com/questions/3552461/how-to-format-a-javascript-date for additional option keys examples.
-  let options = {
-
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    hour: "numeric",
-    minute: "numeric",
-
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
-  res.render("list", {
-    kindOfDay: day,
-    newListItems: items
-  });
+  res.render("list", {listTitle: day,newListItems: items});
 
 });
 
@@ -47,9 +31,23 @@ app.post("/", function(req, res) {
 
   let item = req.body.newItem;
 
-  items.push(item);
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-  res.redirect("/");
+});
+
+// targeting the work page
+app.get("/work", function (req,res){
+  res.render("list", { listTitle: "Work List", newListItems: workItems})
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
 });
 
 app.listen(3000, function() {
